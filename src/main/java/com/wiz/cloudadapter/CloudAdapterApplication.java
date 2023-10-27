25fbcb3ce8bc1a8f1eca0a7397d4c3a660d6dc3b
Saubhagya Dwitiya <saubhagya.carvia@gmail.com>
2023-10-27 15:46:21 +0530

    Initial commit

---
+++ src/main/java/com/wiz/cloudadapter/CloudAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.wiz.cloudadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiz/cloudadapter/CloudAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiz.cloudadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
