e3815f12bd887d7cbc56b634ca365ff6d25b3684
saubhagya <saubhagya.carvia@gmail.com>
2023-11-02 00:28:26 +0530

    oauth flow added

---
+++ src/main/java/com/wiz/cloudadapter/config/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.wiz.cloudadapter.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcuserService = new OidcUserService();
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository(){
+        return new InMemoryClientRegistrationRepository(this.googleClientRegistration());
+    }
+
+
+    private ClientRegistration googleClientRegistration(){
+        return ClientRegistration.withRegistrationId("google")
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .redirectUri(redirectUri)
+                .scope("openid", "profile", "email", "https://www.googleapis.com/auth/drive")
+                .authorizationUri("https://accounts.google.com/o/oauth2/auth")
+                .tokenUri("https://accounts.google.com/o/oauth2/token")
+                .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
+                .userNameAttributeName(IdTokenClaimNames.SUB)
+                .jwkSetUri("https://www.googleapis.com/oauth2/v3/certs")
+                .clientName("Google")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authz -> authz
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .oidcUserService(this.oidcuserService)
+                        )
+                );
+        return http.build();
+    }
+}
--- src/main/java/com/wiz/cloudadapter/gdrive/Controller.java
+++ src/main/java/com/wiz/cloudadapter/gdrive/Controller.java
@@ -1,10 +1,13 @@
 package com.wiz.cloudadapter.gdrive;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.authenticator.SingleSignOnSessionKey;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +17,13 @@ import java.security.Principal;
 @Slf4j
 public class Controller {
 
-    @GetMapping("/")
-    public String message(@AuthenticationPrincipal OAuth2AccessToken token){
+    @GetMapping("/oauth/google/login")
+    public String message(Model model, OAuth2AuthenticationToken token){
+        log.info("token: {}",token);
+        return "success";
+    }
+    @GetMapping("/home")
+    public String home(@AuthenticationPrincipal OAuth2AuthenticationToken token){
         log.info("token: {}",token);
         return "success";
     }
